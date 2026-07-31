5b38fe325f784e1b68af2e553a7b7be23a1a4ab2
Adibcyberion0710 <raianadibalamewu@gmail.com>
2026-08-01 00:50:14 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/for-gublu-baccha/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
